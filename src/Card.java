17f56ca38f1696d0d4faa13dc84e71d19d4e18a8
camillabutler <camilla_butler25@milton.edu>
2023-05-30 11:24:34 -0400

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,12 @@
+public class Card {
+    public String suit;
+    public int value;
+    public String name;
+    public boolean isDealt;
+    public Card(int psuit, int  pname ){
+        if(psuit == 0){
+    suit = "diamond";
+
+        }
+    }
+}
